a428ca20dae4d2a22c6cd80d2ff90711126ca5a2
andruhaRR <andrushizy@yandex.ru>
2025-12-11 10:25:43 +0500

    Получение информации с сайта. пикулев

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace HttpNewsPAT
 {
@@ -10,6 +12,24 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
+            WebRequest request = WebRequest.Create("https://news.permaviat.ru/main");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            using (Stream dataStream = response.GetResponseStream()) 
+            {
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    string responseFromServer = reader.ReadToEnd();
+                    Console.WriteLine(responseFromServer);
+                }
+            }              
+            
+            
+            //reader.Close();
+            //dataStream.Close();
+            //response.Close();
+            Console.Read();
+
         }
     }
 }
